551c8dec0e5465b356641073db9b3aff29987d60
Eko Wijaya <ekowijayawindows@gmail.com>
2020-10-10 16:35:07 +0700

    scss

--- laravel01/resources/views/welcome.blade.php
+++ laravel01/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GEC
                 </div>
 
                 <div class="links">
